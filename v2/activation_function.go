8c8da68f7ed3307736b2ba071aadb98850e75b28
Christoph Stein <christoph.stein@apic.ai>
2023-01-19 18:15:02 +0100

    Implement sigmoid activation function

--- v2/activation_function.go
+++ v2/activation_function.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 type ActivationFunction interface {
 	compute(val float64) float64
 	derive(val float64) float64
@@ -21,4 +23,14 @@ func (_ ReLU) derive(val float64) float64 {
 	}
 
 	return 1.0
+}
+
+type Sigmoid struct{}
+
+func (_ Sigmoid) compute(val float64) float64 {
+	return 1 / (1 + math.Exp(-val))
+}
+
+func (s Sigmoid) derive(val float64) float64 {
+	return s.compute(val) * (1 - s.compute(val))
 }
