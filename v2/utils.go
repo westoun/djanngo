3ba41b67a9b1c70af010e0cc92e301b557f86065
Christoph Stein <christoph.stein@apic.ai>
2023-01-20 11:34:38 +0100

    Fix referential copy

--- v2/utils.go
+++ v2/utils.go
@@ -13,40 +13,56 @@ func sum(values []float64) float64 {
 }
 
 func createEmptyCopy(array interface{}) interface{} {
-	copy1, has1dim := array.([]float64)
+	reference1, has1dim := array.([]float64)
 	if has1dim {
 
-		for i := 0; i < len(copy1); i++ {
-			copy1[i] = 0.0
+		copy := make([]float64, len(reference1))
+
+		for i := 0; i < len(reference1); i++ {
+			copy[i] = 0.0
 		}
 
-		return copy1
+		return copy
 	}
 
-	copy2, has2dim := array.([][]float64)
+	reference2, has2dim := array.([][]float64)
 	if has2dim {
 
-		for i := 0; i < len(copy2); i++ {
-			for j := 0; j < len(copy2[i]); j++ {
-				copy2[i][j] = 0.0
+		copy := make([][]float64, len(reference2))
+
+		for i := 0; i < len(reference2); i++ {
+
+			copy[i] = make([]float64, len(reference2[i]))
+
+			for j := 0; j < len(reference2[i]); j++ {
+				copy[i][j] = 0.0
 
 			}
 		}
 
-		return copy2
+		return copy
 	}
 
-	copy3, has3dim := array.([][][]float64)
+	reference3, has3dim := array.([][][]float64)
 	if has3dim {
-		for i := 0; i < len(copy3); i++ {
-			for j := 0; j < len(copy3[i]); j++ {
-				for k := 0; k < len(copy3[i][j]); k++ {
-					copy3[i][j][k] = 0.0
+
+		copy := make([][][]float64, len(reference3))
+
+		for i := 0; i < len(reference3); i++ {
+
+			copy[i] = make([][]float64, len(reference3[i]))
+
+			for j := 0; j < len(reference3[i]); j++ {
+
+				copy[i][j] = make([]float64, len(reference3[i][j]))
+
+				for k := 0; k < len(reference3[i][j]); k++ {
+					copy[i][j][k] = 0.0
 				}
 			}
 		}
 
-		return copy3
+		return copy
 	}
 
 	errorMessage := fmt.Sprintf("No casting has been implemented for %T", array)
