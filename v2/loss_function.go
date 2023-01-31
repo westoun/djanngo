f0fc627c7b59b9c699e4a013536abde080f70c02
Christoph Stein <christoph.stein@apic.ai>
2023-01-31 15:46:25 +0100

    Add MSE Log Scaled loss function

--- v2/loss_function.go
+++ v2/loss_function.go
@@ -1,6 +1,8 @@
 package main
 
-import "math"
+import (
+	"math"
+)
 
 type LossFunction interface {
 	compute(prediction []float64, target []float64) float64
@@ -27,4 +29,31 @@ func (_ MSE) derive(prediction []float64, target []float64) []float64 {
 	}
 
 	return derivative
+}
+
+type MSELogScaled struct{}
+
+func (_ MSELogScaled) compute(prediction []float64, target []float64) float64 {
+	// taken from https://stats.stackexchange.com/a/519735
+
+	loss := 0.0
+
+	for i := 0; i < len(prediction); i++ {
+		loss += 1/float64(len(prediction))*math.Pow((prediction[i]-target[i]), 2) +
+			5*1/float64(len(prediction))*math.Pow((math.Log(prediction[i]+10e-9)-math.Log(target[i]+10e-9)), 2)
+	}
+
+	return loss
+}
+
+func (_ MSELogScaled) derive(prediction []float64, target []float64) []float64 {
+	derivative := make([]float64, len(prediction))
+
+	for i := 0; i < len(prediction); i++ {
+		derivative[i] = 2/float64(len(prediction))*(prediction[i]-target[i]) +
+			5*2/float64(len(prediction))*1/(prediction[i]+10e-9)*(math.Log(prediction[i]+10e-9)-math.Log(target[i]+10e-9))
+	}
+
+	return derivative
+
 }
